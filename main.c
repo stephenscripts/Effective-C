82436c53cd493e85979e5f898e0a58cc7661c60d
Nnamani StephenScripts <155476794+stephenscripts@users.noreply.github.com>
2024-01-27 00:31:15 +0100

    update

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int, int);
+
 int main(void) {
     if (puts("Hallo Schatz!") == EOF){
         return EXIT_FAILURE;
     }
     double m = 7.0/3.0;
     printf("%f", m);
-    return EXIT_SUCCESS;
+
+    int a = 23;
+    int b = 32;
+
+    swap(a, b);
+    printf("main: a = %d, b = %d\n", a, b);
+    return 0;
 }
